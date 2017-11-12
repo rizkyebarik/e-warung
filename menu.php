aeb00053db3432b3aef2b0b4ca473e39b5c0f319
reeechart <ferdinandusrichard@yahoo.co.id>
2017-11-12 13:42:38 +0700

    add dummy pages for every link in navbar

---
+++ menu.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Menu - e-Warung</title>
+	<meta charset="utf-8">
+ 	<meta name="viewport" content="width=device-width, initial-scale=1">
+ 	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+ 	<link rel="stylesheet" href="css/master.css">
+ 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.0/jquery.min.js"></script>
+ 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+	<?php include_once("navbar.php"); ?>
+	<div class="container">
+		<div class="page-header">
+			<h1>Lorem Ipsum</h1>
+		</div>
+		<div class="body-content">
+			<p>
+				Lorem ipsum dolor sit amet, consectetur adipiscing elit. Vestibulum non sollicitudin felis. Ut feugiat pellentesque lacus, vitae dignissim odio ultricies nec. Ut iaculis cursus bibendum. Etiam vitae tortor augue. Aliquam erat volutpat. Integer eros lorem, dictum non lectus non, bibendum pulvinar leo. Suspendisse pretium, dui ac maximus mattis, dolor nunc ultrices tellus, ac pulvinar mauris elit vitae dolor. Fusce vehicula mauris eget ex volutpat, sit amet iaculis velit venenatis. Mauris dictum tellus eu ipsum venenatis, non pellentesque est mollis. Fusce tristique efficitur lacus tincidunt blandit. Mauris fringilla blandit nisi, quis placerat massa molestie volutpat. Cras elit quam, tristique volutpat velit nec, varius porttitor nibh. Proin sed mattis turpis, vitae varius metus. Phasellus vitae nibh leo.
+			</p>
+			<p>
+				Praesent tincidunt sapien eu augue mattis aliquet. Mauris varius purus in libero luctus, nec pharetra neque cursus. Quisque volutpat at mauris et consequat. Curabitur lacinia efficitur convallis. Quisque varius, felis sit amet interdum dignissim, massa elit laoreet urna, sit amet gravida nulla velit ut leo. Nulla ornare urna eleifend pharetra egestas. Nullam a vulputate augue, et accumsan ante. Mauris sit amet neque vitae dolor mattis ultrices tincidunt ac ante. Cras pellentesque tincidunt ultricies.
+			</p>
+			<p>
+				In nec risus ultricies, euismod erat vitae, dapibus lorem. Morbi eleifend neque sed orci ultrices varius. Sed porta orci ipsum, non hendrerit ligula ultricies sed. Duis pellentesque dui a laoreet iaculis. Aenean vestibulum, magna viverra egestas gravida, sem nisl ultrices augue, in elementum lectus lectus eu tellus. Sed sagittis purus quam, a tempus lectus varius eget. Fusce ex orci, lobortis in odio eu, aliquam sollicitudin est. Donec gravida nec felis vitae euismod. Aliquam semper lacus non commodo pharetra. Integer tincidunt nisl volutpat nulla mattis, eget rhoncus elit faucibus. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Sed posuere, arcu sed elementum tincidunt, nulla justo ultricies arcu, consectetur facilisis ex leo quis leo. Vivamus ornare augue et efficitur placerat. Proin eu tellus metus.
+			</p>
+			<p>
+				Mauris sodales quam lorem, eget cursus turpis iaculis vulputate. Nunc maximus metus eu imperdiet lacinia. Pellentesque ultrices nulla id lorem tempus lobortis. Suspendisse potenti. Pellentesque fermentum, tortor nec cursus pellentesque, quam lectus lobortis turpis, quis tempus eros elit ut dolor. Maecenas blandit elit libero, et molestie purus viverra at. Vivamus eget suscipit mauris, euismod lacinia diam. Integer egestas lacus vel varius efficitur. Cras tristique, nisi id tincidunt aliquet, ipsum dui efficitur ex, sed tempus eros orci sed eros. Vestibulum laoreet suscipit neque, non semper ipsum viverra eget. Aliquam orci metus, interdum eget venenatis sit amet, consectetur ac nibh. Vivamus sit amet urna at tortor eleifend commodo nec porttitor urna. Duis et dignissim ante. Curabitur lacus erat, rutrum quis ligula eget, pretium tempor leo. Etiam fermentum mi diam, sed bibendum mi porttitor vel. Nunc quis nunc justo.
+			</p>
+			<p>
+				Sed egestas turpis ligula, ut malesuada purus malesuada eget. Vivamus pellentesque ex sem, a auctor risus feugiat maximus. Nulla quis nibh pulvinar risus dapibus condimentum et in orci. Sed id justo vel risus posuere convallis vitae eget eros. Donec orci purus, efficitur sed arcu vitae, rhoncus scelerisque massa. Cras non odio non sem placerat dignissim id nec lectus. Nam porta facilisis porttitor. Fusce tellus urna, fermentum quis euismod quis, fringilla at justo. Phasellus ut nisl elit. Aenean sodales sed mi eu hendrerit. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; Quisque auctor aliquam sem, vitae condimentum massa pharetra in. Vivamus id suscipit erat, ut placerat augue.
+			</p>
+		</div>
+	</div>
+</body>
+</html>
---
+++ navbar.php
@@ -0,0 +1,14 @@
+<nav class="navbar navbar-default">
+	<div class="container-fluid">
+		<div class="navbar-header">
+			<img class="small-icon floatleft" src="img/e-warung.png">
+			<a class="navbar-brand" href="#">e-Warung</a><br>
+			<span id="motto">Food on-the-go</span>
+		</div>
+		<ul class="nav navbar-nav floatright">
+			<li><a href="menu.php">Menu</a></li>
+			<li><a href="order.php">Make an Order</a></li>
+			<li><a href="myorders.php">My Orders</a></li>
+		</ul>
+	</div>
+</nav>
